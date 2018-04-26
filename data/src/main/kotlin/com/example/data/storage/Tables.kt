588730d606dd301a0747952bf6e4ba5ffc569e8d
Mikhail <zcmx@ya.ru>
2018-04-26 10:47:15 +0300

    cleaning of tails in a data layer

--- data/src/main/kotlin/com/example/data/storage/Tables.kt
+++ data/src/main/kotlin/com/example/data/storage/Tables.kt
@@ -1,7 +1,5 @@
 package com.example.data.storage
 
 object Tables {
-    const val COURSE_FULL_DATA = "course_full_data"
     const val COURSE_MAIN_DATA = "course_main_data"
-    const val USERS = "users"
 }
